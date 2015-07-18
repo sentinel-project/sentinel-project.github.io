eaae20142ee7f7dd7c384fcb10fd67f514ec9f96
Clinton Dreisbach <clinton@dreisbach.us>
2015-07-18 13:56:58 -0400

    Add Vagrantfile

--- bluehouse/bluehouse/settings.py
+++ bluehouse/bluehouse/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'bluehouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bluehouse',
+        'USER': 'bluehouse',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1'
     }
 }
 

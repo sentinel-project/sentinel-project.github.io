7dfbd2065f68ee6b4427ca9c380ea72c1d2444ed
Clinton Dreisbach <clinton@dreisbach.us>
2015-07-18 18:59:42 +0000

    Added model for Sentinel data

--- bluehouse/bluehouse/settings.py
+++ bluehouse/bluehouse/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'sentinel',
+
+    'django_extensions',
 )
 
 MIDDLEWARE_CLASSES = (

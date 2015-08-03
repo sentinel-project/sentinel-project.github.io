0f64249067818a09b5b726d879e846c802f637b2
Clinton Dreisbach <clinton@dreisbach.us>
2015-08-03 11:05:14 -0400

    Added legend

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,36 @@
 var d3 = require('d3');
-require("d3-geo-projection")(d3);
-var topojson = require('topojson');
 var queue = require('queue-async');
 var _ = require("underscore");
+var colorbrewer = require("colorbrewer");
 
 var width = 1200,
     height = 800;
-//
-// var projection = d3.geo.eckert4()
-//     .scale(220)
-//     .rotate([-10, 0])
-//     .translate([width / 2, height / 2]);
-// var path = d3.geo.path().projection(projection);
-// var graticule = d3.geo.graticule();
-
-// var svg = d3.select("#map").append("svg")
-//     .attr("width", width)
-//     .attr("height", height);
-//
-// svg.append("defs").append("path")
-//     .datum({type: "Sphere"})
-//     .attr("id", "sphere")
-//     .attr("d", path);
-//
-// svg.append("use")
-//     .attr("class", "stroke")
-//     .attr("xlink:href", "#sphere");
-//
-// svg.append("use")
-//     .attr("class", "fill")
-//     .attr("xlink:href", "#sphere");
-//
-// svg.append("path")
-//     .datum(graticule)
-//     .attr("class", "graticule")
-//     .attr("d", path);
 
 var svg = d3.select("#world-map").select("svg")
     .attr("width", width)
     .attr("height", height);
 
 function updateMap(error, dataMap) {
-  var countries = svg.selectAll("path.land");
-  console.log(countries);
+  var scale = d3.scale.quantize()
+      .domain([0, Math.round(_(dataMap.values()).max() / 9) * 9]);
 
-  var quantize = d3.scale.quantize()
-      .domain([0, _(dataMap.values()).max()])
+  var colors = scale.range(colorbrewer.BuGn[9]);
+  var legend = d3.select('#legend')
+    .append('ul')
+    .attr('class', 'list-inline');
+
+  var keys = legend.selectAll('li.key').data(colors.range());
+  keys.enter().append('li')
+      .attr('class', 'key')
+      .style('border-top-color', String)
+      .text(function(d) {
+        var r = colors.invertExtent(d);
+        return r[0];
+      });
+
+  var countries = svg.selectAll("path.land");
+
+  var quantize = scale
       .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
   countries.attr("class", function () {
